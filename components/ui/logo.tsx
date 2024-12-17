37c232113c9d376680aab30661aff956561eba17
emha arch <mhmmdmhthr@gmail.com>
2024-12-17 19:26:40 +0700

    init

---
+++ components/ui/logo.tsx
@@ -0,0 +1,14 @@
+{`'use client';
+
+import { BookOpenCheck } from 'lucide-react';
+
+export const Logo = () => {
+  return (
+    <div className="flex items-center gap-2 text-primary">
+      <BookOpenCheck className="w-8 h-8" />
+      <span className="font-bold text-2xl tracking-tight">
+        Skill<span className="text-[#004F39]">opa</span>
+      </span>
+    </div>
+  );
+};`}
